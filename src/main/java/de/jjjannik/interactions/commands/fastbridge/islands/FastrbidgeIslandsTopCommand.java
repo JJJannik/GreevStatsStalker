cb4be11188a16788db349ce7a44e9e2ef51de1dc
JJJannik <jannikjentsch2006@gmail.com>
2024-07-10 22:08:14 +0200

    Add fastbridge top commands

--- src/main/java/de/jjjannik/interactions/commands/fastbridge/islands/FastrbidgeIslandsTopCommand.java
+++ src/main/java/de/jjjannik/interactions/commands/fastbridge/islands/FastrbidgeIslandsTopCommand.java
@@ -1,11 +1,58 @@
 package de.jjjannik.interactions.commands.fastbridge.islands;
 
-import de.jjjannik.interactions.Interaction;
+import de.jjjannik.classes.TopCommand;
+import de.jjjannik.entities.fastbridge.FastBridgeIslandsPlayer;
+import de.jjjannik.requests.FastbridgeIslands;
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.entities.MessageEmbed;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+import net.dv8tion.jda.api.interactions.commands.OptionMapping;
 
-public class FastrbidgeIslandsTopCommand implements Interaction {
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class FastrbidgeIslandsTopCommand extends TopCommand {
     @Override
     public void execute(SlashCommandInteractionEvent evt) {
+        handleTopCommand(evt, top -> {
+            String map = evt.getOption("map", OptionMapping::getAsString);
 
+            List<FastBridgeIslandsPlayer> topStats = jga.getTopFastbridgeIslands(FastbridgeIslands.Map.valueOf(map), top.amount(), top.offset());
+
+            List<MessageEmbed> embeds = new ArrayList<>();
+
+            EmbedBuilder builder = new EmbedBuilder().setColor(Color.GREEN)
+                    .setTitle("Top %s Fastbridge Island `%s` stats starting at offset %s".formatted(
+                            top.amount(),
+                            map,
+                            top.offset()
+                    ));
+            EmbedBuilder builder1 = new EmbedBuilder().setColor(Color.GREEN);
+
+            for (int i = 0; i < 50; i++) {
+                if (topStats.size() == i) break;
+
+                FastBridgeIslandsPlayer stats = topStats.get(i);
+
+                MessageEmbed.Field field = new MessageEmbed.Field(stats.getName(), """
+                        Time: %s
+                        Replay Id: %s
+                        Timestamp: %s
+                        """.formatted(stats.getTime(), stats.getReplayId(), stats.getTimestamp()), true);
+
+                if (i < 25) {
+                    builder.addField(field);
+                } else {
+                    builder1.addField(field);
+                }
+            }
+            embeds.add(builder.build());
+            if (!builder1.getFields().isEmpty()) {
+                embeds.add(builder1.build());
+            }
+
+            evt.replyEmbeds(embeds).queue();
+        });
     }
 }
--- src/main/java/de/jjjannik/interactions/commands/fastbridge/mode/FastrbidgeModeTopCommand.java
+++ src/main/java/de/jjjannik/interactions/commands/fastbridge/mode/FastrbidgeModeTopCommand.java
@@ -1,11 +1,71 @@
 package de.jjjannik.interactions.commands.fastbridge.mode;
 
+import de.jjjannik.classes.TopCommand;
+import de.jjjannik.entities.fastbridge.FastBridgeIslandsPlayer;
+import de.jjjannik.entities.fastbridge.FastBridgePlayer;
 import de.jjjannik.interactions.Interaction;
+import de.jjjannik.requests.FastbridgeIslands;
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.entities.MessageEmbed;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+import net.dv8tion.jda.api.interactions.commands.OptionMapping;
 
-public class FastrbidgeModeTopCommand implements Interaction {
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+public class FastrbidgeModeTopCommand extends TopCommand {
     @Override
     public void execute(SlashCommandInteractionEvent evt) {
+        handleTopCommand(evt, top -> {
+            String mode = evt.getOption("mode", OptionMapping::getAsString);
 
+            List<FastBridgePlayer> topStats;
+
+            switch (mode) {
+                case "INCLINED" -> topStats = jga.getTopFastbridgeInclined(top.amount(), top.offset());
+                case "INCLINED_SHORT" -> topStats = jga.getTopFastbridgeInclinedShort(top.amount(), top.offset());
+                case "NORMAL" -> topStats = jga.getTopFastbridge(top.amount(), top.offset());
+                case "SHORT" -> topStats = jga.getTopFastbridgeShort(top.amount(), top.offset());
+                case "EXTRA_SHORT" -> topStats = jga.getTopFastbridgeExtraShort(top.amount(), top.offset());
+                case "STAIRCASE" -> topStats = jga.getTopFastbridgeStaircase(top.amount(), top.offset());
+                default -> topStats = null;
+            }
+
+            List<MessageEmbed> embeds = new ArrayList<>();
+
+            EmbedBuilder builder = new EmbedBuilder().setColor(Color.GREEN)
+                    .setTitle("Top %s Fastbridge '%s' stats starting at offset %s".formatted(
+                            top.amount(),
+                            mode,
+                            top.offset()
+                    ));
+            EmbedBuilder builder1 = new EmbedBuilder().setColor(Color.GREEN);
+
+            for (int i = 0; i < 50; i++) {
+                if (topStats.size() == i) break;
+
+                FastBridgePlayer stats = topStats.get(i);
+
+                MessageEmbed.Field field = new MessageEmbed.Field(stats.getName(), """
+                        Time: %s
+                        Replay Id: %s
+                        Timestamp: %s
+                        """.formatted(stats.getTime(), stats.getReplayId(), stats.getTimestamp()), true);
+
+                if (i < 25) {
+                    builder.addField(field);
+                } else {
+                    builder1.addField(field);
+                }
+            }
+            embeds.add(builder.build());
+            if (!builder1.getFields().isEmpty()) {
+                embeds.add(builder1.build());
+            }
+
+            evt.replyEmbeds(embeds).queue();
+        });
     }
 }
